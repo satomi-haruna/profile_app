30b13f4e21c059859edbc2e50be376e3bdc2a344
Ubuntu <ubuntu@ip-172-31-0-64.ap-northeast-1.compute.internal>
2023-06-23 14:43:40 +0000

    Add static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,9 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 end
