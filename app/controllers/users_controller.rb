58071e66af9c2e52a88ac4bb1517f9ba8d98dbbe
Ubuntu <ubuntu@ip-172-31-0-64.ap-northeast-1.compute.internal>
2023-07-05 07:05:10 +0000

    Use SSL and the Puma web server in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
 
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation, :introduction)
+    end
 end
