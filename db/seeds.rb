ff26a1646fe5c14bfa18e8d747868390065c86d5
Ubuntu <ubuntu@ip-172-31-0-64.ap-northeast-1.compute.internal>
2023-07-07 02:23:14 +0000

    Add login, logout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# メインユーザーを登録しておく
+User.create!(name:  "Sato",
+             email: "satomi.haruna1124@gmail.com",
+             password:              "ss4649hh00",
+             password_confirmation: "ss4649hh00",
+             introduction: "自己紹介を入力しましょう")
