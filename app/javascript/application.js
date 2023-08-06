0c4ef3e91af23516689801e2e3d10c83407b63c8
Ubuntu <ubuntu@ip-172-31-0-64.ap-northeast-1.compute.internal>
2023-08-06 04:58:23 +0000

    Fix error turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
 import "controllers"
 import "custom/menu"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
 require("chartkick")
 require("chart.js")
-
-Rails.start()
-  // Turbolinks.start()
-ActiveStorage.start()
-  import "controllers"
